3a9596eff5819574f244f5a15a7a251046cfd0a9
Dnyaneshwar Madhewad <96038212+dnyaneshwar-madhewad@users.noreply.github.com>
2025-04-27 11:49:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -220,6 +220,6 @@ app.post('/single-payment', (req, res) => {
 // Load data and start server
 loadDatabase();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
